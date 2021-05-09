89df714de317921f7eb1a68ca9baef63d003b521
Brandon Rawson <rawsonbr@flip2.engr.oregonstate.edu>
2021-05-09 11:05:27 -0700

    Initial commit to brandon_hdl branch. New files zigzag.sv and quantization.sv that contain basic headers

---
+++ quantization.sv
@@ -0,0 +1,7 @@
+module quantization(input  logic clk,
+		    input  logic start,
+                    input  logic [7:0][7:0][9:0] in_matrix,
+	            output logic [7:0][7:0][9:0] out_matrix,
+	    	    output logic done);
+
+endmodule	
---
+++ zigzag.sv
@@ -0,0 +1,7 @@
+module zigzag(input  logic clk,
+	      input  logic start,
+              input  logic [7:0][7:0][9:0] in_matrix,
+	      output logic [9:0] out_stream,
+	      output logic done);
+
+endmodule	
